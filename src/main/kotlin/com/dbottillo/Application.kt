f13a77f2566ab3054ec79e1729b4f56c77218764
Daniele <dbottillo@gmail.com>
2022-08-20 15:28:02 +0100

    Trying to fix heroku issue

--- src/main/kotlin/com/dbottillo/Application.kt
+++ src/main/kotlin/com/dbottillo/Application.kt
@@ -2,11 +2,8 @@ package com.dbottillo
 
 import com.dbottillo.plugins.*
 import io.ktor.server.application.*
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
-
 @Suppress("unused")
 fun Application.module() {
     configureKoin()
